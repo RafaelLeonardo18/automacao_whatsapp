0f4e7fe51882f37b8374f895313639c2e4a81960
Rafael Leonardo de Lima <rafaelldl.rldl@gmail.com>
2021-06-24 17:53:03 -0300

    Adição de classe de teste do WhatsApp

---
+++ automacao_whatsapp/src/test/java/br/com/yaman/testes/Teste.java
@@ -0,0 +1,33 @@
+package br.com.yaman.testes;
+
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class Teste {
+
+    static AppiumDriver<MobileElement> driver;
+
+    public static void main(String[] args) throws Exception {
+        openWhatsApp();
+    }
+
+    public static void openWhatsApp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Galaxy J2 Pro");
+        capabilities.setCapability("uidid", "8de800bf");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "7.1.1");
+        capabilities.setCapability("appPackage", "com.whatsapp");
+        capabilities.setCapability("appActivity", "com.whatsapp.Conversation");
+        capabilities.setCapability("", "");
+        capabilities.setCapability("", "");
+        URL url = new URL("http://localhost:4723/wd/hub");
+        driver = new AppiumDriver<MobileElement>(url, capabilities);
+        System.out.println("Aplicação iniciada");
+
+    }
+}
